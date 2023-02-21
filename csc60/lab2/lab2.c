c23bc8d91e07f21f762e6abbfeba5449d87acd47
conner-chil32 <fishbacon561@gmail.com>
2023-02-20 18:02:52 -0800

    This is the first commit containing labs 1 and 2 (2/20/2023)

---
+++ csc60/lab2/lab2.c
@@ -0,0 +1,16 @@
+/*-------------------*/
+/*  Conner Childers  */
+/*      Lab 2        */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+    printf("\nLab2. \n\n");
+    printf("Hello World.\n");
+    printf("Hi, Conner Childers. \n\n");
+    printf("Sometimes my genius... It's almost frightening\n");
+    printf("-Jeremy Clarkson\n");
+
+    return EXIT_SUCCESS;
+}
