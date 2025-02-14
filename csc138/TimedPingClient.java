4cee517357ff217d90db0175d931dde1d1e8657d
Conner Childers <fishbacon561@gmail.com>
2025-02-14 12:45:26 -0800

    update 138

--- csc138/TimedPingClient.java
+++ csc138/TimedPingClient.java
@@ -10,14 +10,20 @@ public class TimedPingClient {
         }
         try {
             DatagramSocket socket = new DatagramSocket();
-            long start = System.currentTimeMillis();
             InetAddress address = InetAddress.getByName(args[0]);
             boolean reachable = address.isReachable(5000);
             byte[] data = "test".getBytes();
 
+            long start = System.currentTimeMillis();
+
             if(reachable) {
-                DatagramPacket packet = new DatagramPacket(data, data.length, address, 53);
+                DatagramPacket packet = new DatagramPacket(data, data.length, address, 7);
                 socket.send(packet);
+
+                byte[] buffer = new byte[1024];
+                DatagramPacket recieved_packet = new DatagramPacket(buffer, buffer.length);
+                socket.receive(recieved_packet);
+                
                 socket.close();
                 long end = System.currentTimeMillis();
                 System.out.println(args[0] + " is reachable. RTT: " + (end-start) + "ms");
