45918df013b2b030b3e9676376a1492463e041a3
Conner Childers <fishbacon561@gmail.com>
2025-04-05 20:37:11 -0700

    aaaaaa

---
+++ csc138/tcpccs.java
@@ -0,0 +1,28 @@
+// CLIENT
+
+import java.net.*;
+import java.io.*;
+
+// Written By: Conner Childers, 4/5/2025
+
+public class tcpccs {
+    
+    public static void main(String[] args) {
+        
+    }
+
+    public static class MessageListener extends Thread {
+
+    }
+
+    public static class UserInput extends Thread {
+
+    }
+
+    public static class FileReception extends Thread {
+
+    }
+
+}
+
+
---
+++ csc138/tcpcss.java
@@ -0,0 +1,17 @@
+// SERVER
+
+import java.net.*;
+import java.io.*;
+
+// Written By: Conner Childers, 4/5/2025
+
+public class tcpcss {
+
+    public static void main(String[] args) {
+        
+    }
+
+    public static class ClientHandler extends Thread {
+
+    }
+}
