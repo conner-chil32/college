b2ad5c02393d1fd2c8368a20927eb984d82ccd35
Conner Childers <fishbacon561@gmail.com>
2025-04-05 22:32:24 -0700

    updated 138 proj

--- csc138/tcpccs.java
+++ csc138/tcpccs.java
@@ -1,26 +1,118 @@
 // CLIENT
 
-import java.net.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.Socket;
+import java.net.http.WebSocket.Listener;
+import java.util.Scanner;
 
 // Written By: Conner Childers, 4/5/2025
 
 public class tcpccs {
+
+    private static Socket socket;
+    private static BufferedReader bufferedReader;
+    private static BufferedWriter bufferedWriter;
+    private static String username;
     
     public static void main(String[] args) {
+        if(args.length <= 0) {
+            System.out.println("Usage: java tcpccs.java <hostname> <username>");
+            return;
+        }
+
+        try {
+            socket = new Socket(args[0], 12345);
+            username = args[1];
+            bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            bufferedWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+
+            bufferedWriter.write(username);
+            bufferedWriter.newLine();
+            bufferedWriter.flush();
+
+            System.out.println("Connected to the server. You can start sending messages.");
+
+            UserInput ui = new UserInput();
+            MessageListener ml = new MessageListener();
+            //FileTransfer ft = new FileTransfer();
+
+            Thread userInputThread = new Thread(ui);
+            Thread messageListenerThread = new Thread(ml);
+            //Thread fileTransferThread = new Thread(ft)
+
+            userInputThread.start();
+            messageListenerThread.start();
+            //fileTransferThread.start();
+            
+        } catch(IOException e) {
+            close(socket, bufferedReader, bufferedWriter);
+        }
+    }
+
+    private static void close(Socket socket, BufferedReader bufferedReader, BufferedWriter bufferedWriter) {
+        try {
+            if(bufferedReader != null) {
+                bufferedReader.close();
+            }
+            if(bufferedWriter != null) {
+                bufferedWriter.close();
+            }
+            if(socket != null) {
+                socket.close();
+            }
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static class MessageListener implements Runnable {
+
+        @Override
+        public void run() {
+            String msg;
+
+            while(socket.isConnected()) {
+                try {
+                    msg = bufferedReader.readLine();
+                    System.out.println(msg);
+                } catch(IOException e) {
+                    close(socket, bufferedReader, bufferedWriter);
+                }
+            }
+        }
         
     }
 
-    public static class MessageListener extends Thread {
+    public static class UserInput implements Runnable {
+
+        @Override
+        public void run() {
+           try {
+                Scanner scanner = new Scanner(System.in);
+                while(socket.isConnected()) {
+                    String msg = scanner.nextLine();
+                    bufferedWriter.write("[" + username + "] " + msg);
+                    bufferedWriter.newLine();
+                    bufferedWriter.flush();
+                }
+            } catch(IOException e) {
+                close(socket, bufferedReader, bufferedWriter);
+            }
+        }
 
     }
 
-    public static class UserInput extends Thread {
+    public static class FileTransfer implements Runnable {
 
-    }
+        @Override
+        public void run() {
 
-    public static class FileReception extends Thread {
-
+        }
+        
     }
 
 }
--- csc138/tcpcss.java
+++ csc138/tcpcss.java
@@ -1,17 +1,122 @@
 // SERVER
 
-import java.net.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
 
 // Written By: Conner Childers, 4/5/2025
 
+
 public class tcpcss {
 
-    public static void main(String[] args) {
+    private static ServerSocket serverSocket;
+    private static ArrayList<Thread> threads = new ArrayList<>();
+
+    public static void main(String[] args) throws IOException {
+        int port = args.length == 0 ? 12345 : Integer.valueOf(args[0]);
+        serverSocket = new ServerSocket(port);
+        System.out.println("Listener on port 12345\n" + "Waiting for connections...");
+        try {
+            while(!serverSocket.isClosed()) {
+                Socket socket = serverSocket.accept();
+                System.out.println("New connection, thread name is Thread-" + threads.size() + ", ip is: " + socket.getInetAddress() + ", port is: " + socket.getPort());
+                ClientHandler clientHandler = new ClientHandler(socket);
+                Thread thread = new Thread(clientHandler);
+                thread.start();
+            }
+        } catch(IOException e) {
+
+        }
+    }
+
+    public void StopServer() {
+        try {
+            if(serverSocket != null) {
+                serverSocket.close();
+            }
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static class ClientHandler implements Runnable {
+
+        public static ArrayList<ClientHandler> clients = new ArrayList<>();
+        private Socket socket;
+        private BufferedReader bufferedReader;
+        private BufferedWriter bufferedWriter;
+        private String clientName;
+
+        public ClientHandler(Socket socket) {
+            try {
+                this.socket = socket;
+                this.bufferedWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+                this.bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                this.clientName = bufferedReader.readLine();
+                System.out.println("Adding to list of sockets at " + clients.size());
+                clients.add(this);
+                broadcast("[" + clientName + "] has joined the chat.");
+            } catch(IOException e) {
+                close(socket, bufferedReader, bufferedWriter);
+            }
+        }
+
+        @Override
+        public void run() {
+            String msg;
+            while(socket.isConnected()) {
+                try {
+                    msg = bufferedReader.readLine();
+                    broadcast(msg);
+                } catch(IOException e) {
+                    close(socket, bufferedReader, bufferedWriter);
+                    break;
+                }
+            }
+        }
+
+        public void broadcast(String msg) {
+            System.out.println(msg);
+            for (ClientHandler clientHandler : clients) {
+                try {
+                    if(clientHandler.clientName != clientName) {
+                        clientHandler.bufferedWriter.write(msg);
+                        clientHandler.bufferedWriter.newLine();
+                        clientHandler.bufferedWriter.flush();
+                    }
+                } catch(IOException e) {
+                    close(socket, bufferedReader, bufferedWriter);
+                }
+            }
+        }
+
+        public void removeClientHandler() {
+            clients.remove(this);
+            broadcast("["+clientName+"] has left the chat.");
+        }
+
+        public void close(Socket socket, BufferedReader reader, BufferedWriter writer) {
+            removeClientHandler();
+            try {
+                if(bufferedReader != null) {
+                    bufferedReader.close();
+                }
+                if(bufferedWriter != null) {
+                    bufferedWriter.close();
+                }
+                if(socket != null) {
+                    socket.close();
+                }
+            } catch(IOException e) {
+                e.printStackTrace();
+            }
+        }
         
     }
-
-    public static class ClientHandler extends Thread {
-
-    }
+    
 }
